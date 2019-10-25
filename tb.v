44bf45290e3db2e8cd02551ded2e4dd3660a6b92
chingyiwu <37971669+chingyiwu@users.noreply.github.com>
2019-10-25 11:31:48 +0800

    Update tb.v

--- tb.v
+++ tb.v
@@ -12,79 +12,87 @@
 `define SIM_CYCLE 5000
 //`timescale 100s/1s
 module tb;
-reg   load;
-reg			clock;
+reg   Load;
+reg   Count;
+reg			clk;
 reg		[3:0]	state;
-reg		  in1;
-reg		  in2;
-reg		  in3;
-reg		 	in4;
-reg		 	in5;
-reg		 	in6;
-reg		 	in7;
-reg		 	in8;
-wire   out1;
-wire   out2;
-wire	  out3;
-wire	  out4;
-wire	 	out5;
-wire	 	out6;
-wire	 	out7;
-wire	 	out8;
-
-
+reg		  D0;
+reg		  D1;
+reg		  D2;
+reg		 	D3;
+reg   clean;
+wire   Q0;
+wire   Q1;
+wire	  Q2;
+wire	  Q3;
+wire   CO;
 initial begin
   state=4'b0000;
-  load = 0;
-  in1 = 0;
-  in2 = 0;
-  in3 = 0;
-  in4 = 0;
-  in5 = 0;
-  in6 = 0;
-  in7 = 0;
-  in8 = 0;
+  Load = 0;
+  Count= 0;
+  clean= 0;
+  D0 = 0;
+  D1 = 0;
+  D2 = 0;
+  D3 = 0;
+  #20
+    clean= 1;
   #30
-    in1<=1;     in2<=0;     in3<=1;     in4<=0; 
+    Count=1; 
   #40
-    in5<=0;     in6<=1;     in7<=0;     in8<=1;
+    Count=0;
   #40
-    load<=1;
-  #40  
-    load<=0;
+    Count=1; 
   #40
-    in1<=1;     in2<=1;     in3<=1;     in4<=0;
+    Count=0;
   #40
-    in5<=0;     in6<=0;     in7<=1;     in8<=0;
+    Count=1; 
   #40
-    load<=1;
-  #40  
-    load<=0;
+    Count=0;
+  #40
+    Count=1; 
+  #40
+    Count=0;
+  #40
+    Count=1;
+  #40
+    Count=0;
+  #40
+    Count=1; 
+  #40
+    Count=0;
+  #40
+    Count=1; 
+  #40
+    Count=0;
+  #40
+    Count=1; 
+  #40
+    Count=0;
+  #40
+    Count=1; 
+   #40
+    Count=0;
+  
 end
 always begin
   //@(posedge clk);
-    # 20 clock = 1; 
-    # 20 clock = ~clock;
+    # 20 clk = 1; 
+    # 20 clk = ~clk;
 end
-a1021 m(
-	.in1(in1),
-	.in2(in2),
-	.in3(in3),
-	.in4(in4),
-	.in5(in5),
-	.in6(in6),
-	.in7(in7),
-	.in8(in8),
-	.clock(clock),
-	.load(load),
-	.out1(out1),
-	.out2(out2),
-	.out3(out3),
-	.out4(out4),
-	.out5(out5),
-	.out6(out6),
-	.out7(out7),
-	.out8(out8)
+counter m(
+	.D0(D0),
+	.D1(D1),
+	.D2(D2),
+	.D3(D3),
+	.clk(clk),
+	.Load(Load),
+	.Count(Count),
+	.clean(clean),
+	.Q0(Q0),
+	.Q1(Q1),
+	.Q2(Q2),
+	.Q3(Q3),
+	.CO(CO)
 );
 endmodule
-
